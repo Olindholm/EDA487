37bf6654b442bc13789ce52ca023b7b744055933
Wiggy boy <wiggy0boy@gmail.com>
2017-03-31 12:47:12 +0200

    Created basefile for exercise_1

---
+++ c/exercises_1/src/main.c
@@ -0,0 +1,20 @@
+/* 
+ * Copyright 2017 (C) Osvald Lindholm
+ * 
+ * This file is the result of the exercises provided
+ * for the Maskinorienterad programmering (EDA487)
+ * course at Chalmers. The exercises can be found at:
+ * http://www.cse.chalmers.se/~uffe/MOP/hemuppgifter/hemuppgifter.html
+ * 
+ * This file is written on (and for) Windows, compiled
+ * using the GCC with the language standard C99.
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	return 0;
+}
