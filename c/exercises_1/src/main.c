0415ee58daa363defca52346301e589a10e734b6
Wiggy boy <wiggy0boy@gmail.com>
2017-03-31 14:23:07 +0200

    Exercise_1_3a completed

--- c/exercises_1/src/main.c
+++ c/exercises_1/src/main.c
@@ -17,6 +17,10 @@
 // Function summary
 void printManyTimes(char str[], unsigned int n);
 void printBackwards(char str[]);
+void printByte(unsigned char b);
+void printBytes(unsigned long long l, int n);
+void printShort(unsigned short s);
+void printInt(unsigned int i);
 
 /* 
  * Start of program!
@@ -33,8 +37,16 @@ int main(int argc, char **argv) {
 	 * Exercise_1_2
 	 * Create a string and print it backawards.
 	 */
-	 printBackwards("This text is pretty hard to read");
+	 //printBackwards("This text is pretty hard to read");
 	 
+	 /* 
+	 * Exercise_1_3a
+	 * Create a function printing a byte, short and int bitwise to the console.
+	 */
+	printByte(128);		printf("\n");
+	printShort(30345);	printf("\n");
+	printInt(65987);	printf("\n");
+	
 }
 
 /* 
@@ -75,4 +87,65 @@ void printBackwards(char str[]) {
 	}
 	
 	printf("%s\n", rts);
+}
+
+#define BYTE_LENGTH 8
+
+/* 
+ * Prints a byte to the console bitwise in ones and zeroes.
+ * 
+ * @param b the byte to be printed.
+ */
+void printByte(unsigned char b) {
+	
+	/* 
+	 * Shifts each bit of the byte to most-right bit position,
+	 * starting with the most-left bit. Followed by nullifying
+	 * all other bits and printing the result. Repeated for
+	 * each bit.
+	 */
+	for (int i = 0; i < BYTE_LENGTH; i++) {
+		int bit = (b >> (BYTE_LENGTH - (i+1))) & 0x01;
+		printf("%d", bit);
+	}
+}
+
+/* 
+ * Prints to the console a number of bytes.
+ * 
+ * @param l the bytes consecutively.
+ * @param n the number of bytes to be printed.
+ */
+void printBytes(unsigned long long l, int n) {
+	
+	/* 
+	 * Shifting a byte of bits to the most-right position,
+	 * allows printing for that specific byte.
+	 * 
+	 * We can thus print shorts, and ints easily by specifying
+	 * their lengths by byte, that being 2 and 4 respectively.
+	 * (see printShort(..) and printInt(..) )
+	 */
+	while (n > 0) {
+		n--;
+		printByte((l >> n * BYTE_LENGTH));
+	}
+}
+
+/* 
+ * Prints a short to the console bitwise in ones and zeroes.
+ * 
+ * @param s the short to be printed.
+ */
+void printShort(unsigned short s) {
+	printBytes(s, sizeof(s));
+}
+
+/* 
+ * Prints an int to the console bitwise in ones and zeroes.
+ * 
+ * @param i the int to be printed.
+ */
+void printInt(unsigned int i) {
+	printBytes(i, sizeof(i));
 }
