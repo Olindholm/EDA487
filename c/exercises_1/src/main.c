6f48fbd332c362faa8aa2340106e5298c3c8835a
Wiggy boy <wiggy0boy@gmail.com>
2017-03-31 14:00:24 +0200

    Exercise_1_2 completed

--- c/exercises_1/src/main.c
+++ c/exercises_1/src/main.c
@@ -12,8 +12,11 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
 // Function summary
+void printManyTimes(char str[], unsigned int n);
+void printBackwards(char str[]);
 
 /* 
  * Start of program!
@@ -24,8 +27,14 @@ int main(int argc, char **argv) {
 	 * Exercise_1_1
 	 * Create a foor-loop printing "I am an awesome C programmer" 10 times.
 	 */
-	printManyTimes("I am an awesome C programmer\n", 10);
+	//printManyTimes("I am an awesome C programmer\n", 10);
 	
+	/* 
+	 * Exercise_1_2
+	 * Create a string and print it backawards.
+	 */
+	 printBackwards("This text is pretty hard to read");
+	 
 }
 
 /* 
@@ -39,4 +48,31 @@ void printManyTimes(char str[], unsigned int n) {
 		printf(str);
 		n--;
 	}
+}
+
+/* 
+ * Prints a string backwards to the console.
+ * 
+ * @param str the string to be printed.
+ */
+void printBackwards(char str[]) {
+	int length = strlen(str);
+	
+	/* 
+	 * strlen(str) returns only the length of the string.
+	 * The string also needs to be exited with a null character
+	 * at the end. Thus, (length + 1).
+	 */
+	char rts[length + 1];
+	rts[length] = '\0';
+	
+	/*
+	 * Loops through the str, inserting each character
+	 * into rts, backwards of course.
+	 */
+	for (int i = 0; i < length; i++) {
+		rts[length - (i+1)] = str[i];
+	}
+	
+	printf("%s\n", rts);
 }
