47e16a76997f5685d7f0d44758623b9218a56d87
Wiggy boy <wiggy0boy@gmail.com>
2017-03-31 13:56:49 +0200

    Exercise_1_1 completed

--- c/exercises_1/src/main.c
+++ c/exercises_1/src/main.c
@@ -13,8 +13,30 @@
 
 #include <stdio.h>
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-	return 0;
+// Function summary
+
+/* 
+ * Start of program!
+ */
+int main(int argc, char **argv) {
+	
+	/* 
+	 * Exercise_1_1
+	 * Create a foor-loop printing "I am an awesome C programmer" 10 times.
+	 */
+	printManyTimes("I am an awesome C programmer\n", 10);
+	
+}
+
+/* 
+ * Prints a message to the console multiple times.
+ * 
+ * @param str the message to be printed.
+ * @param n the number of times to print the message.
+ */
+void printManyTimes(char str[], unsigned int n) {
+	while (n > 0) {
+		printf(str);
+		n--;
+	}
 }
