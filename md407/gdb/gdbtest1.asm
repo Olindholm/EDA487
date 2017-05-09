c7f117b1bdef940222263cf37ae660aa80cf957e
Wiggy boy <wiggy0boy@gmail.com>
2017-05-09 17:46:42 +0200

    Lab assignment 1.4 completed

---
+++ md407/gdb/gdbtest1.asm
@@ -0,0 +1,18 @@
+@
+@ Copyright 2017 (C) Osvald Lindholm
+@
+@ This file is the result of the lab excercises provided
+@ for the Maskinorienterad programmering (EDA407)
+@ course at Chalmers. The lab exercises can be found at:
+@ http://www.cse.chalmers.se/edu/year/2017/course/EDA482_7/Documents/LabPM-MOP-2017-02-18.pdf
+@
+@ This file is written for the MD407 lab computer
+@ using an ARM V6-m processor.
+@ Detailed specficiations can be found at:
+@ http://www.cse.chalmers.se/edu/resources/mop/documents/MD407_beskrivning.pdf
+@ http://www.cse.chalmers.se/edu/year/2017/course/EDA482_7/Documents/quickguide-mop.pdf
+@ 
+
+start:	MOV	R0,#0
+main:	ADD	R0,R0,#1
+	B	main
